7a4effc515952492f5f0b1449c722027b1cc90b3
Jacob450 <jacob676763@gmail.com>
2026-05-08 23:04:39 -0400

    added Textures
    End of video 6

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+
+
 
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+#include"Texture.h"
 
+//------- Ignore this ----------
+#include<filesystem>
+namespace fs = std::filesystem;
+//------------------------------
 
 
 
 int main() {
+
+
 	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -18,21 +28,19 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-	// Vertices coordinates              / color values
+	// Vertices coordinates / color values    // texture coordinates
 	GLfloat vertices[] = {
-   -0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f, 0.02f,// Lower Left Corner
-	0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f, 0.02f, // Lower Right Corner
-	0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.3f, // upper Corner
-   -0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f,// inner left
-	0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f, // inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3,      0.0f, 0.8f, 0.3f, 0.02f // inner right
+   -0.5f, -0.5f, 0.00f,      1.0f, 0.0f, 0.0f,   0.0f, 0.0f,// Lower Left Corner
+   -0.5f,  0.5f, 0.00f,      0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // Lower Right Corner
+	0.5f,  0.5f, 0.00f,      0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // upper Corner
+	0.5f, -0.5f, 0.00f,      1.0f, 1.0f, 1.0f,   1.0f, 0.0f// inner left
+	
 
 	};
 
 	GLuint indeces[]{
-		0, 3, 5, // Lower Left triangle
-		3, 2, 4, // Lower right triangle
-		5, 4, 1  // upper right triangle
+		0, 2, 1, // first triangle
+		0, 3, 2 // second triangle
 	};
 
 	//create window
@@ -61,16 +69,29 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indeces, sizeof(indeces));
 	//links VBO to Vao so that opengl knows how to read the VBO and use it in the shader
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.UnBind();
 	VBO1.UnBind();
 	EBO1.UnBind();
 
-	
+	// assigns a value to the uniform (scale) from the vertex shader 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 
+
+
+	//texture  
+	Texture context("C:\\Users\\Jacob\\Downloads\\context.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	context.texUnit(shaderProgram, "tex0", 0);
+
+
+
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 	//loop so the window stays open
 	while (!glfwWindowShouldClose(window)) {
@@ -78,16 +99,20 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		//Tell opengl which shader program to use
 		shaderProgram.Activate();
+		//update the value of our uniform (scale) from our shader 
 		glUniform1f(uniID, 0.5f);
+		context.Bind();
 		//Bind the VAO so opengl knows to use it
 		VAO1.Bind();
 
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		glfwSwapBuffers(window);
 		//proccess window events
 		glfwPollEvents();
+
+		
 	}
 
 	//delete all the object we created
@@ -95,6 +120,7 @@ int main() {
 	VBO1.Delete();
 	EBO1.Delete();
 	shaderProgram.Delete();
+	context.Delete();
 	
 
 	//destroy window when done
--- default.vert
+++ default.vert
@@ -1,10 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTex;
 
 // outputs the color for the fragment shader
 out vec3 color;
 
+out vec2 texCoord;
+
+//controls scale of vertices
 uniform float scale;
 
 void main()
@@ -12,4 +16,5 @@ void main()
 	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
 	// Assign the colors from the vertex data to "color" which will be used in the fragment shader"
 	color = aColor;
+	texCoord = vec2(aTex.x, aTex.y) ;
 }
